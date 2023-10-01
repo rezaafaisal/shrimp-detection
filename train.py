8be6e45b82b92f90238adfb9afb4f89b9b6466d2
rezaafaisal <rezha.faizal567@gmail.com>
2023-10-01 14:20:11 +0800

    fix directory

--- train.py
+++ train.py
@@ -28,8 +28,8 @@ train_data = object_detector.DataLoader.from_pascal_voc(
 )
 
 val_data = object_detector.DataLoader.from_pascal_voc(
-    'dataset/val',
-    'dataset/val',
+    'dataset/valid',
+    'dataset/valid',
     ['1']
 )
 
